d909916e7c998b5a66fce7a8cb2bcbdc4e708e43
mridmtoji <mridmtoji@gmail.com>
2025-12-11 20:25:15 +0700

    Niri: wallpaper changer scripts supports videos

--- .config/niri/scripts/changeWallpaper.sh
+++ .config/niri/scripts/changeWallpaper.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
-
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 CACHE_WALL="$HOME/.cache/current_wallpaper"
 
-SELECTED=$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' -o -iname '*.gif' \) \
+SELECTED=$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' -o -iname '*.gif' -o -iname '*.mp4' \) \
   | walker --dmenu "Choose wallpaper:")
 
 [ -z "$SELECTED" ] && exit 0
 
-MODE=$(printf "light\ndark\n" | walker --dmenu "Matugen mode:")
-[ -z "$MODE" ] && exit 0
+EXT="${SELECTED##*.}"
+EXT_LOWER=$(echo "$EXT" | tr '[:upper:]' '[:lower:]')
 
-ln -sf "$SELECTED" "$CACHE_WALL"
-
-matugen image "$SELECTED" --mode "$MODE"
-
-# pkill awww 2>/dev/null
-
-awww img "$CACHE_WALL" &
+if [ "$EXT_LOWER" = "mp4" ]; then
+  pkill mpvpaper 2>/dev/null
+  
+  mpvpaper -o "no-audio loop" ALL "$SELECTED" &
+  
+  ln -sf "$SELECTED" "$CACHE_WALL"
+else
+  MODE=$(printf "light\ndark\n" | walker --dmenu "Matugen mode:")
+  [ -z "$MODE" ] && exit 0
+  
+  ln -sf "$SELECTED" "$CACHE_WALL"
+  matugen image "$SELECTED" --mode "$MODE"
+  
+  pkill mpvpaper 2>/dev/null
+  
+  awww img "$CACHE_WALL" &
+fi
